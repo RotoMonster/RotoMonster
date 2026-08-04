c0d6496fb67b9a322cf595db633e86ef96e29d32
Rylie DuVal <rylieduval10@gmail.com>
2026-08-04 09:36:22 -0500

    Add PageTitleRow Razor partial and wire into DepthCharts

---
+++ RotoMonster/Models/Shared/PageTitleRowViewModel.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace RotoMonster.Models.Shared
+{
+    public class PageTitleRowViewModel
+    {
+        public string Title { get; set; }
+
+        public SelectList UserLeagueList { get; set; }
+        public int SelectedUserLeagueId { get; set; }
+        public string LeagueFieldName { get; set; } = "SelectedUserLeagueId";
+
+        public bool ShowRefreshRosters { get; set; }
+        public string RefreshHandler { get; set; } = "Refresh";
+
+        public bool ShowPlayerSearch { get; set; }
+        public string PlayerSearchId { get; set; } = "pageTitleRowSearch";
+        public string PlayerSearchPlaceholder { get; set; } = "Search players";
+    }
+}
